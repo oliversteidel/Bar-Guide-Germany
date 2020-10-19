5cdf0ab50e36c46311019996335e683e6de91036
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2020-10-19 21:30:43 +0200

    changes to nav-dropdown-menu
    -styling & position changes
    -fixed issue with activating dropdown
     by clicking below town-link
    -cleaning code

--- animate.js
+++ animate.js
@@ -1,11 +1,8 @@
 
-
-
-
 $(document).ready(function () {
-    // $('.line-1').css('transform', 'translateY(0)');
-    // $('.line-2').css('transform', 'translateY(0)');
-    // $('.line-3').css('transform', 'translateY(0)');
+
+    //fade in the 3 lines of text with delay
+    
     var delayTimeWelcome = 1;
     for (var i = 1; i <= 3; i++) {
         gsap.to(".line-" + i, {
@@ -16,25 +13,31 @@ $(document).ready(function () {
         delayTimeWelcome += 0.5;
     }
 
+    //fade out the welcome-screen by opacity of 100%
+
     gsap.to(".welcome-screen", {
         delay: 3,
         duration: 2,
         opacity: 0,
     });
 
+    //setting welcome-screen to display: none
+    //to avoid laying on top of any buttons
+
     gsap.to(".welcome-screen", {
         delay: 5,
-
         display: 'none'
     });
+
+    //animation of nav-dropdown
+
     var closed = true;
     $('.town').click(function () {
-        console.log(closed);
         
-
         var delayTime = 0;
 
-        if (closed) {
+        if (closed) {            
+
             for (var i = 1; i <= 19; i++) {
                 gsap.to(".nd-item-" + i, {
                     duration: 0.3,
@@ -45,100 +48,24 @@ $(document).ready(function () {
             }
             closed = false;
         } else {
+
             for (var i = 19; i >= 1; i--) {
                 gsap.to(".nd-item-" + i, {
                     duration: 0.3,
-                    x: 160,
+                    x: 180,
                     delay: delayTime
                 });
                 delayTime += 0.05;
             }
             closed = true;
         }
-
-
     });
-
 });
 
 
 
 
 
-var dropdownClosed = true;
-
-
-
-
-
-
-
-
-
-$(document).ready(function () {
-    // $('.line-1').css('transform', 'translateY(0)');
-    // $('.line-2').css('transform', 'translateY(0)');
-    // $('.line-3').css('transform', 'translateY(0)');
-    var delayTimeWelcome = 1;
-    for (var i = 1; i <= 3; i++) {
-        gsap.to(".line-" + i, {
-            duration: 0.9,
-            y: 0,
-            delay: delayTimeWelcome
-        });
-        delayTimeWelcome += 0.5;
-    }
-
-    gsap.to(".welcome-screen", {
-        delay: 3,
-        duration: 2,
-        opacity: 0,
-    });
-
-    gsap.to(".welcome-screen", {
-        delay: 5,
-
-        display: 'none'
-    });
-    var closed = true;
-    $('.town').click(function () {
-        console.log(closed);
-        
-
-        var delayTime = 0;
-
-        if (closed) {
-            for (var i = 1; i <= 19; i++) {
-                gsap.to(".nd-item-" + i, {
-                    duration: 0.3,
-                    x: 0,
-                    delay: delayTime
-                });
-                delayTime += 0.05;
-            }
-            closed = false;
-        } else {
-            for (var i = 19; i >= 1; i--) {
-                gsap.to(".nd-item-" + i, {
-                    duration: 0.3,
-                    x: 160,
-                    delay: delayTime
-                });
-                delayTime += 0.05;
-            }
-            closed = true;
-        }
-
-
-    });
-
-});
-
-
-
-
-
-var dropdownClosed = true;
 
 
 
